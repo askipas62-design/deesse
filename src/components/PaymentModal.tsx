31a18217529244b3a736496aaf4b6b83d60ff173
askipas62-design <askipas62@gmail.com>
2026-05-22 16:18:21 +0000

    Update PaymentModal.tsx

--- src/components/PaymentModal.tsx
+++ src/components/PaymentModal.tsx
@@ -92,15 +92,15 @@ export default function PaymentModal({
       fields: [
         {
           label: "IBAN",
-          value: "FR76 1723 8000 0100 4146 8128 515"
+          value: "FR76 1621 8000 0140 1213 1466 927"
         },
         {
           label: "BIC",
-          value: "SCSYFRP2"
+          value: "BFBKFRP1"
         },
         {
           label: "Nom du Bénéficiaire",
-          value: "Madisonne marjolaine crucon"
+          value: "Jennifer Pelliccia"
         }
       ],
       instruction:
